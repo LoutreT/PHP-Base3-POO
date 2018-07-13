dfe40d27feb23b6f9df63a612b561308e87f780d
Vincent <v.nassaux@nootos.com>
2018-07-14 01:15:00 +0200

    Connection entre Atom et Github

--- OpenClassRoomJuillet2018/Chapitre2/PremierEssai.php
+++ OpenClassRoomJuillet2018/Chapitre2/PremierEssai.php
@@ -1,8 +1,17 @@
 <?php
 
+
 class Personnage
 {
+  private $_degats = 0 ;
   private $_experience = 50 ;
+  private $_force = 20 ;
+
+
+  private function frapper($persoAfrapper)
+  {
+    $persoAFrapper->_degats += $this->_force;
+  }
 
   private function afficherExperience()
   {
@@ -11,12 +20,21 @@ class Personnage
 
   private function gagnerExperience()
   {
-    $this->_experience = $this->_experience + 1;
+    $this->_experience = $this->_experience + 1 ;
   }
 }
 
-$perso = new Personnage;
-$perso->gagnerExperience();   // on gagne de l'expérience.
-$perso->afficherExperience(); // on affiche la nouvelle valeur de l'attribut.
+
+$perso = new Personnage ;
+
+$perso->gagnerExperience() ;   // on gagne de l'expérience.
+$perso->afficherExperience() ; // on affiche la nouvelle valeur de l'attribut.
+
+
+$perso01 = new Personnage ;
+$perso02 = new Personnage ;
+
+$perso01->frapper($perso02) ;
+
 
 ?>
