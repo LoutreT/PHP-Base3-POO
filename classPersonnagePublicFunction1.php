2085cdebe3c5bb64b262fc39cdf8bae28e237fc5
Vincent <v.nassaux@nootos.com>
2018-07-13 03:49:20 +0200

    1ière phase-1ier chapitre

---
+++ classPersonnagePublicFunction1.php
@@ -0,0 +1,32 @@
+<?php
+
+// auteur Vincent Nx (LoutreT)
+
+class Personnage            // Voilà ici je créée une classe, un moule
+{
+  private $_force;          // et ici je crée des niveaux de propriétés
+  private $_localisation;   // Propriété de niveau private.
+  private $_experience;
+  private $_degats;
+
+
+  public function deplacer()  // ici je crée une méthode, une fonction.
+  {
+
+  }
+
+
+  public function frapper()   // ici je crée une seconde méthode, une fonction.
+  {
+
+  }
+
+
+  public function gagnerExperience()  // ici je crée une 3e méthode, une fonction
+  {
+
+  }
+
+}
+
+?>
