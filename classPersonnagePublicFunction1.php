efb968854374eae005fe0b4a9b52598246af5d45
Vincent <v.nassaux@nootos.com>
2018-07-13 04:25:35 +0200

    Starter Box

--- classPersonnagePublicFunction1.php
+++ classPersonnagePublicFunction1.php
@@ -1,32 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Réentrainement PHP-POOclass</title>
+  </head>
+  <body>
+
+  <?php
+
 
 // auteur Vincent Nx (LoutreT)
 
-class Personnage            // Voilà ici je créée une classe, un moule
-{
-  private $_force;          // et ici je crée des niveaux de propriétés
-  private $_localisation;   // Propriété de niveau private.
-  private $_experience;
-  private $_degats;
+
+    class Personnage            // Voilà ici je créée une classe, un moule
+    {
+      private $_force;          // et ici je crée des niveaux de propriétés
+      private $_localisation;   // Propriété de niveau private.
+      private $_experience;
+      private $_degats;
 
 
-  public function deplacer()  // ici je crée une méthode, une fonction.
-  {
+      public function deplacer()  // ici je crée une méthode, une fonction.
+      {
 
-  }
-
-
-  public function frapper()   // ici je crée une seconde méthode, une fonction.
-  {
-
-  }
+      }
 
 
-  public function gagnerExperience()  // ici je crée une 3e méthode, une fonction
-  {
+      public function frapper()   // ici je crée une seconde méthode, une fonction.
+      {
 
-  }
+      }
 
-}
 
-?>
+      public function gagnerExperience()  // ici je crée une 3e méthode, une fonction
+      {
+
+      }
+
+    }
+
+  ?>
+
+  </body>
+</html>
