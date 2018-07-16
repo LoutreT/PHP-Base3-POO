87ba8a90641736997f53f5dc9d41035f3d216328
Vincent <v.nassaux@nootos.com>
2018-07-16 02:15:16 +0200

    fonctions accesseurs

--- revision15juillet.php
+++ revision15juillet.php
@@ -7,29 +7,62 @@
   </head>
   <body>
 
-
-
-
+     <!--
+     De ce que j'ai vu, il faut mettre dans l'ordre suivant les groupes :
+        - Attribut avec leur propriété (public-private-protected)
+        - Fonction avec leur propriété (public-private-protected)
+        - Mutateur, pour transformer la valeur de l'attribut.
+        - Accesseur pour
+     -->
 
     <?php
       class Personnage
       {
-        public $_attaquer = 30;
-        public $_degats = 20;
+        private $_force;
+        private $_experience;
+        private $_degats;
 
-        public function frapper(Personnage PersoAFrapper)
+        public function frapper(Personnage $PersoAFrapper)
         {
-          $this->_attaquer = $this->_attaquer + 5 ;
+          $PersoAFrapper->_degats += $this->_force ;
         }
 
-                                 // L'ACCESSEUR permet de
-        public function degats() // accesseur permettant d'utiliser la valeur...
-                                 // ...de l'attribut degats au sein de la classe.
+        public function gagner()
+        {
+          $this->_experience++;
+        }
+
+
+        public function setForce($force)  // ici setForce permet de muter, c'est
+        {                                 //  un MUTATEUR
+          return $this->_force
+        }
+
+                                 // L'ACCESSEUR (Getters) permet de
+        public function force() // accesseur permettant d'utiliser la valeur...
+        {                        // ...de l'attribut degats au sein de la classe.
+          return $this->_force;
+        }
+
+        public function experience()
+        {
+          return $this->_experience;
+        }
+
+        public function degats()
         {
           return $this->_degats;
         }
+
       }
       $perso01 = new Personnage
+      $perso02 = new Personnage
+
+      $perso01->gagner()
+      $perso02->setForce(10); // ici la force vaut 10
+
+      $perso02->gagner()
+
       $perso01->gagner()
     ?>
 
