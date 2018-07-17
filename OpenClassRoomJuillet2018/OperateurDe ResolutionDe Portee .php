3a12d8bf52d1e695b20bfd326b693de8539cfa14
Vincent <v.nassaux@nootos.com>
2018-07-17 03:05:00 +0200

    Amélioration à imprimer pour moi

--- OpenClassRoomJuillet2018/OperateurDe ResolutionDe Portee .php
+++ OpenClassRoomJuillet2018/OperateurDe ResolutionDe Portee .php
@@ -7,7 +7,6 @@
   <body>
 
     <?php
-
       class Personnage
       {
 ////////   ATTRIBUTS    ////////////////////////////////////////////////////////
@@ -23,40 +22,35 @@
 
 ////////   VARIABLE STATIQUE    ////////////////////////////////////////////////
         private static $_texteADire ="Je découvre la fonction
-        statique dans le chapitre 3";
-
+                                      statique dans le chapitre 3";
 ////////   __CONSTRUCT (iniatilisateur)    /////////////////////////////////////
         public function __construct($forceinitiale)
         {
           $this->setForce($forceinitiale);
-          // $this->setDegats($degats);
-          // $this->_experience = 1;
         }
 
 ////////   FUNCTION    /////////////////////////////////////////////////////////
         public function frapper(Personnage $PersoAFrapper)
         {
-          // $PersoAFrapper->_degats += $this->_force;
+
         }
 
 ////////   MUTATEUR    /////////////////////////////////////////////////////////
         public function setForce($force)
         {
-          if(!in_array($force,[self::FORCE_PETITE, self::FORCE_MOYENNE,
-          self::FORCE_GRANDE]))
+          if(!in_array($force,[self::FORCE_PETITE,
+                               self::FORCE_MOYENNE,
+                               self::FORCE_GRANDE]
+            ))
           {
               $this->_force = $force;
           }
         }
-
         public static function parler()
         {
           echo self::$_texteADire;
         }
       }
-
     ?>
-
-
   </body>
 </html>
