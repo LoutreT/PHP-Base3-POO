abd07e5b88140df20c12b7db5a503546158c641f
Vincent <v.nassaux@nootos.com>
2018-07-18 03:07:15 +0200

    Structure d'entrainement pour le CRUD de PDO (CRUD = Create, Read, Update, Delete)

--- OpenClassRoomJuillet2018/Chapitre4/ManagerPourBDD.php
+++ OpenClassRoomJuillet2018/Chapitre4/ManagerPourBDD.php
@@ -7,6 +7,53 @@
   <body>
 
     <?php
+      class PersonnageManager
+      {
+        private $_db;  /* Attribut, c'est ici qu'on instancie pour la PDO - BDD
+                       db signifiant data base */
+
+        public function __construct($db)
+        {
+          $this->setDb($Db); /* Lieu d'initialisation des données car Construct
+                             initialise les attributs placés en haut*/
+        }
+
+        public function add(Personnage $perso)
+        {
+          // Insertion de requête
+          // Assignation des valeurs (id, nom, date, force, degats, experience,...)
+          // Execution de la requête (return ?)
+        }
+
+        public function delete(Personnage $perso)
+        {
+          // (PDO, Execute une requête de type DELETE)
+        }
+
+        public function get($id)
+        {
+          /* Execute une requête de type SELECT avec une clause WHERE,
+          et retourne */
+        }
+
+        public function getList()
+        {
+          /* retournela liste de tous les personnages enregistrés dans le
+          tableaux de la PDO */
+        }
+
+        public function update(Personnage $perso)
+        {
+          // Prépare une requête de type UPDATE.(modification / mise à jour)
+          // Assignation des valeurs modifiées pour la requête.
+          // Execution de la requête.(return ?)
+        }
+
+        public function setDb(PDO $db)
+        {
+          $this->_db = $db;
+        }
+      }
 
     ?>
 
